345903e77641c4da62cb9534dc833f123557c9fb
Emil O'Brien <ejobrien714@gmail.com>
2016-02-08 09:13:23 -0500

    First

---
+++ Reviewer/Controllers/BreweriesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reviewer.Controllers
+{
+    public class BreweriesController : Controller
+    {
+        // GET: Breweries
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowTable()
+        {
+
+            return null;
+        }
+
+        public ActionResult ShowList()
+        {
+
+            return null;
+        }
+    }
+}
